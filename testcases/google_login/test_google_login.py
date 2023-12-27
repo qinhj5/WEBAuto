de77e6b88c0f5ed1d37ab42d23ec7a504eaf5108
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2023-12-27 17:40:36 +0800

    Update test_google_login.py

--- testcases/google_login/test_google_login.py
+++ testcases/google_login/test_google_login.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+import allure
 import pytest
 from utils import get_json_data
 
 
+@allure.severity("normal")
+@pytest.mark.normal
 @pytest.mark.parametrize("email", ["null"])
 def test_google_login_error_account_using_parameter(google_login_page, email):
     google_login_page.enter_email(email=email)
     assert google_login_page.check_email_not_found_prompt()
 
 
+@allure.severity("normal")
+@pytest.mark.normal
 def test_google_login_error_account_using_data(google_login_page):
     data = get_json_data(json_name="account")
     google_login_page.enter_email(email=data["email"])
